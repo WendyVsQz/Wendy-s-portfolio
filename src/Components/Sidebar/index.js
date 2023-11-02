5f62320751fd4d43339893220abd8bbc26dce260
Wendy <118736912+WendyVsQz@users.noreply.github.com>
2023-11-02 18:49:03 +0000

    navbar layout

--- src/Components/Sidebar/index.js
+++ src/Components/Sidebar/index.js
@@ -1,15 +1,15 @@
 import { Link, NavLink } from 'react-router-dom'
 import './index.scss'
-import LogoS from '../../Assets/images/logo-s.png'
-import LogoSubtitle from '../../Assets/images/logo_sub.png'
+import LogoW from '../../Assets/images/VV.png'
+import LogoSubtitle from '../../Assets/images/coollogo_com-31020544.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons'
+import { faEnvelope, faHome, faUser} from '@fortawesome/free-solid-svg-icons'
 
 const Sidebar =()=> (
     <div className='nav-bar'>
     <Link className='logo' to= '/'>
-        <img src={LogoS} alt='logo'/>
-        <img className='sub-logo' src={LogoSubtitle} alt='slobodan'/>
+        <img src={LogoW} alt='logo'/>
+        <img className='sub-logo' src={LogoSubtitle} alt='Winter Veiro'/>
         </Link>
         <nav>
             <NavLink exact="true" activeclassname="activate" to= "/">
